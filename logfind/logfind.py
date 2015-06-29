10ca9a9a90f560d5e26a2d0db3a3824be82626ad
Milan Vojnovic <milanvojnovic@gmail.com>
2015-06-29 08:26:58 +0200

    playing around with 'argparse'

--- logfind/logfind.py
+++ logfind/logfind.py
@@ -1,1 +1,11 @@
-print "logfind"
+import argparse
+
+parser = argparse.ArgumentParser(description='Finds string(s) in log files. Takes any number of arguments.')
+parser.add_argument('strings', metavar='N', type=str, nargs='+',
+                   help='a string(s) to look for')
+parser.add_argument('-o', dest='logical OR', action='store_const',
+                   const=sum, default=max,
+                   help='switch logical operator to OR (AND is default)')
+
+args = parser.parse_args()
+print args.accumulate(args.integers)
